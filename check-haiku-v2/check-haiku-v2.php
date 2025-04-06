09112e50e7461b3c686c73e61d5cfff6fe3a2a2f
Stefan Utamaru <github.le5ke@stenyan.jp>
2025-04-06 17:46:38 +0900

    コメントで状況をメモ

--- check-haiku-v2/check-haiku-v2.php
+++ check-haiku-v2/check-haiku-v2.php
@@ -1,3 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
+// 形態素解析を活用して、俳句の音数を判定する
+// 検討した選択肢: php-mecab, igo-php, Yahoo日本語形態素解析API, RakutenMA API, Google Cloud Natural Language API
+// オフラインかつ導入が楽なものとしてigo-php (のフォークであるlogue/igo-php)を選択します。
